6f4e6801eea32559e65209e26070c3e2ed427a8d
Kashish Gupta <128106947+kashgupta01@users.noreply.github.com>
2026-04-27 11:16:34 -0400

    Activation functions implementation

---
+++ diff_new_data_training_silu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#$ -P ds596
+#$ -N train_squidiff_diff_data
+#$ -pe omp 1
+#$ -l gpus=1
+#$ -l gpu_compute_capability=7.0
+#$ -M kashishg@bu.edu
+#$ -m e
+
+module load miniconda
+module load cuda/12.8
+conda activate /projectnb/ds596/projects/Team_8/squid_diff_env
+cd "/projectnb/ds596/projects/Team 8/Squidiff"
+
+python train_squidiff.py \
+--logger_path /projectnb/ds596/projects/Team\ 8/activation_extension \
+--data_path /projectnb/ds596/projects/Team\ 8/activation_extension/squidiff_train.h5ad \
+--gene_size 200 \
+--output_dim 200 \
+--activation silu
